7d9f9a4261db9f4b372fc91217edd167b983113a
Aimee <aimeex3@gmail.com>
2017-10-04 07:15:16 -0700

    udpate

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-// server any file in public 
+// serve any file in public 
 app.use(express.static('public'));
 
 app.listen(5000, () => {
